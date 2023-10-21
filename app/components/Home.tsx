798bbfe489dd629b80259038defba0ef5f2e90fd
psmuler <zhmuler@gmail.com>
2023-10-21 18:55:43 +0900

    delete deprecated import

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import React, { useState } from "react";
-import { Button, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import Body from "@/app/components/Body";
-import RepoList from "@/app/components/RepoList";
 import axios from "axios";
-import NextTiptapEditor from "./NextTiptapEditor";
 
 const documents = [
   { id: 1, name: 'Document 1', title: 'The greatest document in the world', roomName: 'room1', doc: 'doc1', owner: 'IiUSAJhdkIbBuUca1UV44zATkrl2' },
@@ -14,39 +12,7 @@ const documents = [
 ];
 
 const Home = () => {
-  // const classes = useStyles();
-  // const [isConnected, setIsConnected] = useState(false);
-  // const [selectedDocument, setSelectedDocument] = useState(null); // 選択されたドキュメント情報を保持
-  // const [docid, setdocid] = useState(3);
-  // ドキュメントが選択された際に呼ばれる関数
-  // const handleDocumentSelect = (documentInfo: any) => {
-  //   setSelectedDocument(documentInfo);
-  // };
   const selectedDocument = documents[0]
-
-  // const CreateNewRepo = (id: any) => {
-  //   const url = "https://us-central1-yjs-editor.cloudfunctions.net/addMessage";
-  //   const requestData = {
-  //     id: docid,
-  //     roomName: "room" + docid,
-  //     name: "repository" + docid,
-  //     title: "The greatest document in the world",
-  //   };
-  //   console.log(requestData);
-  //   axios
-  //     .post(url, requestData)
-  //     .then((response) => {
-  //       // リクエスト成功時の処理
-  //       console.log(response.data);
-  //     })
-  //     .catch((error) => {
-  //       // エラー時の処理
-  //       console.error(error);
-  //     });
-
-  //   setdocid(docid + 1);
-  // };
-
 
   return (
     <div className="flex items-center">
@@ -57,6 +23,7 @@ const Home = () => {
           ) : null}
         </Grid>
         <Grid item xs={2}>
+          <h1>contributors</h1>
         </Grid>
       </Grid>
     </div>
--- app/components/RepositoryActionBar.tsx
+++ app/components/RepositoryActionBar.tsx
@@ -1,32 +1,14 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import HomeIcon from "@mui/icons-material/Home";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { SearchInput } from "@/app/components/custom/searchInput";
-
-import MobileDropdownMenu from "./custom/mobileDropdownList";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import Link from "next/link";
-// import { UserAuth } from "@/app/api/AuthContext";
-import ProfileMenu from "@/app/components/custom/profileMenu";
 import { UserAuth } from '../api/AuthContext';
 import { ActionButton } from "./custom/actionButton";
-import { BeakerIcon, GitPullRequestClosedIcon, GitPullRequestIcon, IssueOpenedIcon, NoteIcon } from "@primer/octicons-react";
+import { GitPullRequestIcon, IssueOpenedIcon, NoteIcon } from "@primer/octicons-react";
 
 
-const menuList = [
-  {
-    name: "Login",
-    url: "/login",
-  },
-  {
-    name: "Sign In",
-    url: "/sign-in",
-  },
-];
 
 const RepositoryActionBar = () => {
   const { user, logOut } = UserAuth();
@@ -70,9 +52,6 @@ const RepositoryActionBar = () => {
             </>
           )}
         </div>
-      </div>
-      <div className="md:hidden flex items-center justify-center px-5">
-        <MobileDropdownMenu menuList={menuList} />
       </div>
     </div>
   );
