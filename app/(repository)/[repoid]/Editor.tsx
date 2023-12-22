89efb61b695796178f47f250b36a4cfc3a00fee0
psmuler <94701070+psmuler@users.noreply.github.com>
2023-12-22 20:57:16 +0900

    web socket test

--- app/(repository)/[repoid]/Editor.tsx
+++ app/(repository)/[repoid]/Editor.tsx
@@ -5,12 +5,7 @@ import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
 import { getRandomUser } from "@/lib/randomUser";
 import * as Y from "yjs";
-import { Button } from "@/app/components/ui/button";
 import { IndexeddbPersistence } from 'y-indexeddb'
-import { rtdb } from "@/app/api/firebase";
-import { ref, push } from "firebase/database"
-import { db } from "@/app/api/firebase";
-import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const ydoc = new Y.Doc();
 const { WebsocketProvider } = require("y-websocket");
@@ -21,10 +16,8 @@ interface Props {
 }
 export default function Editor({ params }: Props) {
     const provider = collaboration ?
-        new WebsocketProvider("wss://34.83.179.84", params.repoid, ydoc) :
+        new WebsocketProvider("wss://io.gnt.place", "", ydoc) :
         new IndexeddbPersistence(params.repoid, ydoc);
-
-    const postListRef = ref(rtdb, params.repoid);
 
     const editor: BlockNoteEditor | null = useBlockNote({
         collaboration: {
@@ -36,39 +29,6 @@ export default function Editor({ params }: Props) {
 
     return (
         <div>
-            <Button onClick={async () => {
-                const docRef = doc(db, "repos", params.repoid);
-                getDoc(docRef).then((latestdoc) => {
-                    const data = latestdoc.data()
-
-                    if (data) {
-                        console.log(data.state)
-                        const u8a = new Uint8Array(atob(data.state).split('').map(char => char.charCodeAt(0)));
-                        Y.applyUpdate(ydoc, u8a)
-                    }
-                })
-            }}>Pull</Button>
-            <Button onClick={async () => {
-                const stateVec = Y.encodeStateAsUpdate(ydoc)
-                const state = btoa(String.fromCharCode.apply(null, Array.from(stateVec)));
-                const date = new Date()
-                await push(postListRef, {
-                    status: "review",
-                    stateVec: state,
-                    comment: "",
-                    createdAt: date.getTime()
-                });
-            }}>Send Letter</Button>
-
-            <Button onClick={async () => {
-                const stateVec = Y.encodeStateAsUpdate(ydoc)
-                const state = btoa(String.fromCharCode.apply(null, Array.from(stateVec)));
-                const date = new Date()
-                await setDoc(doc(db, "repos", params.repoid), {
-                    state: state,
-                    timestamp: date.getTime()
-                });
-            }}>Push</Button>
             <BlockNoteView editor={editor} />
         </div>
     )
