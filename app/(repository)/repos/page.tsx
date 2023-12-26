26070fca79ccb44bb379f10490fcfe2358389cac
psmuler <94701070+psmuler@users.noreply.github.com>
2023-12-26 16:27:50 +0900

    disable create repo button when user not logged in

--- app/(repository)/repos/page.tsx
+++ app/(repository)/repos/page.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { DocumentData, collection, orderBy, startAt } from "firebase/firestore";
-import { query, getDocs } from "firebase/firestore";
+'use client'
+
+import { UserAuth } from "@/app/api/AuthContext";
 import { db } from "@/app/api/firebase";
-import { Button, Table } from '@radix-ui/themes';
-import Link from 'next/link';
+import { Avatar, Button, Table, Tooltip } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
+import { DocumentData, collection, getDocs, orderBy, query, startAt } from "firebase/firestore";
+import Link from 'next/link';
 
 interface Repo {
     id: string,
     data: DocumentData
 }
 const RepositoryListPage = async () => {
+    const { user } = UserAuth();
     const citiesRef = collection(db, "repos");
 
     const q = query(citiesRef, orderBy("title"), startAt(20));
@@ -23,10 +25,17 @@ const RepositoryListPage = async () => {
         }
     ))
 
-
     return (
         <div>
-            <Button ><Link href="/repos/new">Create your own</Link></Button>
+            <Button disabled={user === null}>{user === null ? (
+                <div>
+                    <Tooltip content="Login to Create Your Repo">
+                        <span>Create your own</span>
+                    </Tooltip>
+                </div>
+            ) : (
+                <Link href="/repos/new">Create your own</Link>
+            )} </Button>
             <Table.Root>
                 <Table.Header>
                     <Table.Row>
@@ -40,7 +49,13 @@ const RepositoryListPage = async () => {
                     {repos.map((repo: Repo) => (
                         <Table.Row key={repo.id}>
                             <Table.Cell><Link href={`./${repo.id}`}>{repo.data.title}</Link></Table.Cell>
-                            <Table.Cell>{repo.data.owner}</Table.Cell>
+                            <Table.Cell>{repo.data.users ?
+                                <Avatar
+                                    src={repo.data.users[0].avatarURL}
+                                    fallback="?"
+                                />
+                                : "Unknown"
+                            }</Table.Cell>
                             <Table.Cell></Table.Cell>
 
                             <Table.Cell>
